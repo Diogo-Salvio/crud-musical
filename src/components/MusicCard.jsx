c6d78abdece31377ee1f302db8fe61eb1d9b37df
Diogo-Salvio <diogosalvio787@gmail.com>
2025-10-07 16:38:56 -0300

    feat: função para editar as músicas já editadas.

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,30 +1,70 @@
-import { Card, CardContent, Typography, IconButton , Box} from "@mui/material"
-import DeleteIcon from "@mui/icons-material/Delete";
+import { Card, CardContent, Typography, IconButton, Box, TextField} from "@mui/material"
+import DeleteIcon from "@mui/icons-material/Delete"
+import EditIcon from '@mui/icons-material/Edit'
+import CheckIcon from '@mui/icons-material/Check';
+import { useState } from "react"
 
 
-const MusicCard = ({ musicLink, musicName, id, setMusics}) => {
+const MusicCard = ({ musicLink, musicName, id, setMusics }) => {
+
+    const [editState, setEditState] = useState(false)
 
     const handleDelete = () => {
-            setMusics(prev => prev.filter(music => music.id !== id ))
+        setMusics(prev => prev.filter(music => music.id !== id))
+    }
+
+    const handleChange = (event) => {
+        setMusics((prev) => prev.map((music) => music.id === id ? {...music, [event.target.name]: event.target.value} : music))
     }
 
     return (
         <Card sx={{ width: "80%" }}>
-            <CardContent>
-                <Box display="flex" alignItems="center" justifyContent="space-between">
-                    <Box>
-                        <Typography variant="h6" component="h5">
-                            {musicName}
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            {musicLink}
-                        </Typography>
+            {editState ?
+                <CardContent>
+                    <Box display="flex" alignItems="center" justifyContent="space-between">
+                        <Box sx={{ display: "flex", flexDirection: "column", gap: "10px"}}>
+                            <TextField
+                                id="musicName"
+                                name="musicName"
+                                size="small"
+                                value={musicName}
+                                onChange={handleChange}
+                            />
+                            <TextField
+                                id="musicLink"
+                                name="musicLink"
+                                size="small"
+                                value={musicLink}
+                                onChange={handleChange}
+                            />
+                        </Box>
+                        <IconButton sx={{ color: '#2bff00ff' }}>
+                            <CheckIcon onClick={() => setEditState(false)} />
+                        </IconButton>
+
                     </Box>
-                    <IconButton onClick={handleDelete} color="error">
-                        <DeleteIcon />
-                    </IconButton>
-                </Box>
-            </CardContent>
+                </CardContent>
+                :
+                <CardContent>
+                    <Box display="flex" alignItems="center" justifyContent="space-between">
+                        <Box>
+                            <Typography variant="h6" component="h5">
+                                {musicName}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                {musicLink}
+                            </Typography>
+                        </Box>
+                        <Box>
+                            <IconButton onClick={handleDelete} color="error">
+                                <DeleteIcon />
+                            </IconButton>
+                            <IconButton onClick={() => setEditState(true)}>
+                                <EditIcon />
+                            </IconButton>
+                        </Box>
+                    </Box>
+                </CardContent>}
         </Card>
     )
 }
